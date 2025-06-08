6909852183460edaebfc7111d85f7199f42f52f6
Spicyminion <1legoshark@gmail.com>
2025-06-08 11:18:58 -0400

    update

--- network.py
+++ network.py
@@ -3,11 +3,13 @@ import socket
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "192.168.68.109" # needs to be same as defined in the server file
+        self.server = "192.168.1.95" # needs to be same as defined in the server file
         self.port = 5555
         self.addr = (self.server, self.port)
-        self.id = self.connect()
-        print(self.id)
+        self.pos = self.connect()
+
+    def getPos(self):
+        return self.pos
 
     def connect(self):
         try:
@@ -23,6 +25,3 @@ class Network:
         except socket.error as e:
             print(e)
 
-n = Network()
-print(n.send("hello"))
-print(n.send("working"))
--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import socket
 from _thread import *
 import sys
 
-server = "192.168.68.109"
+server = "192.168.1.95"  # use ipconfig in command prompt to find IP address
 port = 5555
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -15,7 +15,16 @@ except socket.error as e:
 s.listen(4)
 print("waiting for connection, server stated")
 
-def threaded_client(conn):
+def read_pos(str):
+    str = str.split(',')
+    return int(str[0]), int(str[1])  # return two values
+
+def make_pos(tup):
+    return str(tup[0]) + "," + str(tup[1])
+
+pos = [(0, 0), (100, 100)]
+
+def threaded_client(conn, player):
     conn.send(str.encode("Connected"))
     reply = ""
     while True:
@@ -34,9 +43,9 @@ def threaded_client(conn):
             break
     conn.close()
 
-client_id = 0
+currentPlayer = 0
 while True:
     conn, addr = s.accept()
     print("Connected to:", addr)
-    start_new_thread(threaded_client, (conn,))
-    client_id += 1
+    start_new_thread(threaded_client, (conn, currentPlayer))
+    currentPlayer += 1
