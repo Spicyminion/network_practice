b14867ec91f42d90bd449efc0afdf2443c2571a0
Spicyminion <75585835+Spicyminion@users.noreply.github.com>
2025-06-08 14:21:42 -0400

    update

--- server.py
+++ server.py
@@ -1,27 +1,62 @@
 import socket
+from _thread import *
+import sys
+
+server = "192.168.1.186"
+port = 5555
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((server, port))
+except socket.error as e:
+    str(e)
+
+s.listen(2)
+print("Waiting for a connection, Server Started")
+
+def read_pos(str):
+    str = str.split(",")
+    return int(str[0]), int(str[1])
 
 
-class Network:
-    def __init__(self):
-        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "YOUR LOCAL IPV4"
-        self.port = 5555
-        self.addr = (self.server, self.port)
-        self.pos = self.connect()
+def make_pos(tup):
+    return str(tup[0]) + "," + str(tup[1])
 
-    def getPos(self):
-        return self.pos
+pos = [(0,0),(100,100)]
 
-    def connect(self):
+def threaded_client(conn, player):
+    conn.send(str.encode(make_pos(pos[player])))
+    reply = ""
+    while True:
         try:
-            self.client.connect(self.addr)
-            return self.client.recv(2048).decode()
+            data = read_pos(conn.recv(2048).decode())
+            pos[player] = data
+
+            if not data:
+                print("Disconnected")
+                break
+            else:
+                if player == 1:
+                    reply = pos[0]
+                else:
+                    reply = pos[1]
+
+                print("Received: ", data)
+                print("Sending : ", reply)
+
+            conn.sendall(str.encode(make_pos(reply)))
         except:
-            pass
+            break
 
-    def send(self, data):
-        try:
-            self.client.send(str.encode(data))
-            return self.client.recv(2048).decode()
-        except socket.error as e:
-            print(e)
+    print("Lost connection")
+    conn.close()
+
+currentPlayer = 0
+while True:
+    conn, addr = s.accept()
+    print("Connected to:", addr)
+
+    start_new_thread(threaded_client, (conn, currentPlayer))
+    currentPlayer += 1
+
